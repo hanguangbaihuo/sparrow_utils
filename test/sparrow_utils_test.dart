623a052f68714897cb4a2d66f716989adf674957
hbshun <bb@bbyy.me>
2020-07-05 18:16:15 +0800

    remove flutter, just dart

--- test/sparrow_utils_test.dart
+++ test/sparrow_utils_test.dart
@@ -1,7 +1,16 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 
 import 'package:sparrow_utils/sparrow_utils.dart';
 
+enum Season { Spring, Summer, Autumn, Winter }
+
 void main() {
-  test('adds one to input values', () {});
+  test('enum To String', () {
+    assert(HGUtils.enumToString(Season.Autumn) == 'Autumn');
+  });
+
+  test('String to Enum', () {
+    assert(HGUtils.enumFromString<Season>(Season.values, 'Spring') ==
+        Season.Spring);
+  });
 }
