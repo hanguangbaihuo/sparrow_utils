1db38858097f9876329cce9ef2666a0d8d45180a
hbshun <bb@bbyy.me>
2020-07-05 16:14:01 +0800

    first commit

--- lib/sparrow_utils.dart
+++ lib/sparrow_utils.dart
@@ -1,7 +1,40 @@
 library sparrow_utils;
 
+import 'package:intl/intl.dart' show DateFormat;
+
+import 'package:enum_to_string/enum_to_string.dart';
+
+export 'package:intl/intl.dart' show DateFormat;
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+}
+
+class HGUtils {
+  // map
+  static Iterable<E> map<E, T>(
+      Iterable<T> items, E Function(int index, T item) fn) sync* {
+    var index = 0;
+
+    for (final item in items) {
+      yield fn(index, item);
+      index = index + 1;
+    }
+  }
+
+  // static get DateFormat => DateFormat;
+
+  static formatDate(DateTime dt, String format) {
+    return DateFormat(format).format(dt);
+  }
+
+  static String enumToString(enumItem) {
+    return EnumToString.parse(enumItem);
+  }
+
+  static T enumFromString<T>(List<T> enumValues, String str) {
+    return EnumToString.fromString(enumValues, str);
+  }
 }
